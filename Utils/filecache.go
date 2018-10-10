634c6a989698b9278299f63e3878702bc5d7d3e9
Argusdusty <argusdusty@gmail.com>
2018-10-09 22:54:43 -0400

    Recovery for FileCache failures when you try loading while the file is being written

--- Utils/filecache.go
+++ Utils/filecache.go
@@ -24,35 +24,48 @@ var (
 
 // Load a file from the FileCache. If the file is not in the cache, save it there.
 // No memory limit: Be aware of your memory requirements when using this.
-func LoadFileCache(file string, parser func(io.Reader) interface{}) (interface{}, time.Time) {
-	fclock.RLock()
-	fc_raw, ok := FileCache.Load(file)
-	fclock.RUnlock()
-	var load bool
-	if !ok {
-		load = true
-	} else {
-		fc := fc_raw.(FileCacheObject)
-		stat, err := os.Stat(file)
-		if err != nil || (stat.ModTime().After(fc.Time)) {
+func LoadFileCache(file string, parser func(io.Reader) interface{}) (data interface{}, t time.Time) {
+	f := func() {
+		fclock.RLock()
+		fc_raw, ok := FileCache.Load(file)
+		fclock.RUnlock()
+		var load bool
+		if !ok {
 			load = true
+		} else {
+			fc := fc_raw.(FileCacheObject)
+			stat, err := os.Stat(file)
+			if err != nil || (stat.ModTime().After(fc.Time)) {
+				load = true
+			}
+		}
+		if load {
+			f, err := os.Open(file)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
+			stat, err := f.Stat()
+			if err != nil {
+				panic(err)
+			}
+			data = parser(f)
+			t = stat.ModTime()
+			FileCache.Store(file, FileCacheObject{Data: data, Time: t})
+			return
+		} else {
+			fc := fc_raw.(FileCacheObject)
+			data = fc.Data
+			t = fc.Time
+			return
 		}
 	}
-	if load {
-		f, err := os.Open(file)
-		if err != nil {
-			panic(err)
+	defer func() {
+		if r := recover(); r != nil {
+			time.Sleep(5 * time.Second) // May have been in the process of writing. Give it one more try
+			f()
 		}
-		defer f.Close()
-		stat, err := f.Stat()
-		if err != nil {
-			panic(err)
-		}
-		data := parser(f)
-		FileCache.Store(file, FileCacheObject{Data: data, Time: stat.ModTime()})
-		return data, stat.ModTime()
-	} else {
-		fc := fc_raw.(FileCacheObject)
-		return fc.Data, fc.Time
-	}
+	}()
+	f()
+	return
 }
